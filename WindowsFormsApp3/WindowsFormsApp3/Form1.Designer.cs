0f33b2aefeeb2065961ecfc122cc95a310af9350
nguyendat135 <nguyendatdtqn@outlook.com>
2023-05-26 23:30:37 +0700

    Winform

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAsk = new System.Windows.Forms.Button();
+            this.progress = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Location = new System.Drawing.Point(298, 237);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(695, 34);
+            this.txtQuestion.TabIndex = 0;
+            this.txtQuestion.TextChanged += new System.EventHandler(this.txtQuestion_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(24, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bạn hãy hỏi gì đó: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnAsk
+            // 
+            this.btnAsk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnAsk.Font = new System.Drawing.Font("Arial Black", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAsk.ForeColor = System.Drawing.Color.Purple;
+            this.btnAsk.Location = new System.Drawing.Point(542, 433);
+            this.btnAsk.Name = "btnAsk";
+            this.btnAsk.Size = new System.Drawing.Size(109, 44);
+            this.btnAsk.TabIndex = 3;
+            this.btnAsk.Text = "Hỏi";
+            this.btnAsk.UseVisualStyleBackColor = false;
+            this.btnAsk.Click += new System.EventHandler(this.btnAsk_Click);
+            // 
+            // progress
+            // 
+            this.progress.Location = new System.Drawing.Point(298, 92);
+            this.progress.Name = "progress";
+            this.progress.Size = new System.Drawing.Size(695, 23);
+            this.progress.TabIndex = 4;
+            this.progress.Click += new System.EventHandler(this.progress_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(24, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tiến trình: ";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1175, 607);
+            this.Controls.Add(this.progress);
+            this.Controls.Add(this.btnAsk);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuestion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAsk;
+        private System.Windows.Forms.ProgressBar progress;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private const string ApiUrl = "http://localhost:5000/ask"; // Địa chỉ Flask API
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAsk_Click(object sender, EventArgs e)
+        {
+            string question = txtQuestion.Text;
+
+            if (string.IsNullOrEmpty(question))
+            {
+                MessageBox.Show("Hãy nhập câu hỏi!");
+                return;
+            }
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var parameters = new Dictionary<string, string>
+                    {
+                        { "question", question }
+                    };
+
+                    var content = new FormUrlEncodedContent(parameters);
+
+                    // Hiển thị ProgressBar và gửi câu hỏi tới Flask API
+                    progress.Minimum = 0;
+                    progress.Maximum = 100;
+                    progress.Value = 0;
+
+                    var response = await client.PostAsync(ApiUrl, content);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+
+                    // Hiển thị câu trả lời trên MessageBox
+                    DialogResult result = MessageBox.Show(responseContent, "Câu trả lời", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    if (result == DialogResult.OK)
+                    {
+                        txtQuestion.Text = "";
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+        }
+
+        private void txtAnswer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtQuestion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progress_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
